195e89038ea6c16415c8251daf723af745e714c4
陈梦楠 <cncmn@hotmail.com>
2018-07-28 13:20:08 +0800

    android

--- HandwritingRecognization/openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ HandwritingRecognization/openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Git\\Chinese-number-gestures-recognition-master\\DigitalGestureRecognition\\openCVLibrary340\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Git\\handwriting_recognization\\HandwritingRecognization\\openCVLibrary340\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
